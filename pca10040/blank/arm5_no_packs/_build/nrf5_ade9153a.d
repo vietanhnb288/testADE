50b0e9cdfc29537cc03137f97404daf58142b9c1
unknown <vietanhnb288@gmail.com>
2021-06-21 18:31:45 +0700

    test spi

---
+++ pca10040/blank/arm5_no_packs/_build/nrf5_ade9153a.d
@@ -0,0 +1,67 @@
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\..\NRF5_Minh_ThS\User_library\nrf5_ade9153a.c
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\hal\nrf_gpio.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\nrfx.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\integration\nrfx\nrfx_config.h
+.\_build\nrf5_ade9153a.o: ..\config\sdk_config.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\drivers/nrfx_common.h
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf52.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\system_nrf52.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf52_bitfields.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf51_to_nrf52.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf52_name_change.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\compiler_abstraction.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf_peripherals.h
+.\_build\nrf5_ade9153a.o: C:\Users\Dell Pricision 351o\AppData\Local\Arm\Packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.35.0\Device\Include\nrf52832_peripherals.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\integration\nrfx\nrfx_glue.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\integration\nrfx\legacy/apply_old_config.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\soc/nrfx_irqs.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\soc/nrfx_irqs_nrf52832.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\nrf_assert.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\app_util.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\nordic_common.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\app_util_platform.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\app_error.h
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\sdk_errors.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_error.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\app_error_weak.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\soc/nrfx_coredep.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\soc/nrfx_atomic.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\nrfx.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\sdk_resources.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\drivers/nrfx_errors.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\integration\nrfx\legacy\nrf_drv_spi.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\drivers\include\nrfx_spim.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\hal/nrf_spim.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\drivers\include\nrfx_spi.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\modules\nrfx\hal/nrf_spi.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\delay\nrf_delay.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\sdk_common.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\sdk_os.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\util\sdk_macros.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\experimental_section_vars\nrf_section.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\strerror\nrf_strerror.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\src\nrf_log_internal.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_error.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_instance.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_types.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_ctrl.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\src\nrf_log_ctrl_internal.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_backend_interface.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\memobj\nrf_memobj.h
+.\_build\nrf5_ade9153a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\balloc\nrf_balloc.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_default_backends.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\..\NRF5_Minh_ThS\User_library\include\ade9153a.h
+.\_build\nrf5_ade9153a.o: ..\..\..\..\..\..\..\NRF5_Minh_ThS\User_library\include\nrf5_ade9153a.h
